2208b73d695ab6a52712d323b9b68176fd133ecc
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-05-31 22:01:20 +0600

    set layout

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashsboard
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../pages/App";
+import AuthLayout from "../layout/AuthLayout";
+import DashboardLayout from "../layout/DashboardLayout";
+import App from "../pages/auth/App";
+import Dashboard from "../pages/dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/",
+            element: <App />,
+          },
+        ],
+      },
+      {
+        path: "/dashboard",
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
