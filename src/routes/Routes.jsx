56853486e99313d84757965fb334c90ccb70cae0
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-05-31 22:23:35 +0600

    create error page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import AuthLayout from "../layout/AuthLayout";
 import DashboardLayout from "../layout/DashboardLayout";
 import App from "../pages/auth/App";
 import Dashboard from "../pages/dashboard/Dashboard";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AuthLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
