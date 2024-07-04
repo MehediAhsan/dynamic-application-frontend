bbff34f1ce57a40589173c1780769e1b54908d43
MehediAhsan <mdmehedicse01@gmail.com>
2024-07-04 17:58:47 +0600

    updated

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -4,7 +4,7 @@ import DashboardTopNav from "../components/shared/DashboardTopNav";
 
 const DashboardLayout = () => {
   return (
-    <div className="flex lg:gap-5 p-5 bg-[#1C2B3E] text-gray-200 min-h-screen">
+    <div className="flex lg:gap-5 p-5 bg-[#111c28] text-gray-200 min-h-screen">
       <DashboardSideNav />
       <div className="flex flex-col gap-5 w-[100vw] lg:w-[calc(100vw-280px)] transition-all duration-500">
         <DashboardTopNav />
