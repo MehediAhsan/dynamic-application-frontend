e3f9ba127e3dfc724594d0715d0484c0a1861fe1
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-07-05 23:41:19 +0600

    updated side nav

--- src/components/shared/DashboardSideNav.jsx
+++ src/components/shared/DashboardSideNav.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
+import { SideNavOpenContext } from "../../context/SiveNavOpenProvider";
 
 const DashboardSideNav = () => {
-    return (
-        <div className="bg-[#172437] rounded min-h-screen w-[280px] hidden lg:block">
-            <h1 className="mx-4 py-6 text-3xl font-semibold italic tracking-widest border-b border-gray-500">Logo</h1>
-        </div>
-    );
+  const { sideNavOpen } = useContext(SideNavOpenContext);
+
+  return (
+    <div
+      className={`bg-[rgb(23,36,55)] rounded min-h-screen w-[280px] lg:block ${
+        sideNavOpen
+          ? "fixed top-0 left-0 transition-all duration-300 translate-x-0"
+          : "hidden"
+      }`}
+    >
+      <h1 className="mx-4 py-6 text-3xl font-semibold italic tracking-widest border-b border-gray-500">
+        Logo
+      </h1>
+    </div>
+  );
 };
 
 export default DashboardSideNav;
--- src/components/shared/DashboardTopNav.jsx
+++ src/components/shared/DashboardTopNav.jsx
@@ -3,20 +3,24 @@ import { FaBarsStaggered } from "react-icons/fa6";
 import { SideNavOpenContext } from "../../context/SiveNavOpenProvider";
 
 const DashboardTopNav = () => {
-    const {sideNavOpen, setSideNavOpen} = useContext(SideNavOpenContext);
+  const { sideNavOpen, setSideNavOpen } = useContext(SideNavOpenContext);
 
-    console.log(sideNavOpen);
-    return (
-        <div className="bg-[#172437] rounded flex justify-between p-5">
-            <h1 className="text-2xl font-semibold">Title</h1>
-            <div className="flex gap-2 items-center">
-                <span><FaBarsStaggered /></span>
-                <span>Profile</span>
-                <span>Profile</span>
-                <span>Profile</span>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-[#172437] rounded flex justify-between p-5">
+      <h1 className="text-2xl font-semibold">Title</h1>
+      <div className="flex gap-2 items-center">
+        <span>
+          <FaBarsStaggered
+            className="cursor-pointer block lg:hidden"
+            onClick={() => setSideNavOpen(!sideNavOpen)}
+          />
+        </span>
+        <span>Profile</span>
+        <span>Profile</span>
+        <span>Profile</span>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardTopNav;
